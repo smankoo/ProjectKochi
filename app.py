fb5275a17b7489ce5f20a41977fcb4dbd267ffdc
Sumeet Singh <sumeet.mankoo@gmail.com>
2020-06-26 19:17:51 -0400

    starting kochi

--- app.py
+++ app.py
@@ -1,68 +1,47 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from __future__ import unicode_literals
+import youtube_dl
 
-from datetime import datetime
+from flask import Flask, render_template, request, redirect
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+    if request.method == 'GET':
+        return(render_template('index.html'))
+    elif request.method == 'POST':
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'logger': MyLogger(),
+            'progress_hooks': [my_hook],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
 
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return(request.form['url'])
+    
 
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was an issue deleting your task'
-
-@app.route('/update/<int:id>', methods=['POST', 'GET'])
-def update(id):
-    task_to_update = Todo.query.get_or_404(id)
-    if request.method == 'POST':
-        task_to_update.content = request.form['content']    
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an error updating your task"
-        
-    else:
-        return render_template('update.html', task=task_to_update)
-
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
