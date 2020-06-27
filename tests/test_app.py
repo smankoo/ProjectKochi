2d3c2af88455d86c2d1223904b346c50f94995a6
Sumeet Singh <sumeet.mankoo@gmail.com>
2020-06-27 09:11:39 -0400

    added download test

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,8 +15,23 @@ def app():
 def client(app):
     return app.test_client()
 
-
 def test_index(client):
     response = client.get('/')
     assert b'<input' in response.data
     assert b'type="submit"' in response.data
+
+def test_download(client):
+    response = client.post('/', data={'url':'https://www.youtube.com/watch?v=xP5joP3Ba4cQ'})
+    assert response is not None
+    assert response.status_code == 200
+    assert response.content_type == 'audio/mpeg'
+    assert response.content_length == 3130657
+    return(response)
+
+if __name__ == '__main__':
+    app = create_app()
+    client = app.test_client()
+    r = test_download(client)
+    print('Done')
+
+    
